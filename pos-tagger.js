8f2f484bd18cc05da4ea23a3af1423e1df20315d
Prateek Saxena <prtksxna@gmail.com>
2020-05-19 17:07:31 +0530

    chore: include wink-nlp

--- pos-tagger.js
+++ pos-tagger.js
@@ -1,8 +1,15 @@
 const tagger = require('wink-pos-tagger')();
+const winkNLP = require('wink-nlp');
+const its = require( 'wink-nlp/src/its.js' );
+const model = require('wink-eng-lite-model');
+const nlp = winkNLP(model);
+
+
 
 module.exports = (req, res) => {
+  var doc = nlp.readDoc(req.query.sentence);
   res.json({
     sentence: req.query.sentence,
-    tags: tagger.tagSentence(req.query.sentence)
+    tags: doc.tokens().out(its.type)
   })
 }
