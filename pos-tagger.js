8b3069db07dc290baa574f64cebac6b01a7c68ef
Prateek Saxena <prtksxna@gmail.com>
2020-05-21 17:17:34 +0530

    feat(pos-tagger): use wink-nlp

--- pos-tagger.js
+++ pos-tagger.js
@@ -1,15 +1,67 @@
 const tagger = require('wink-pos-tagger')();
 const winkNLP = require('wink-nlp');
 const its = require( 'wink-nlp/src/its.js' );
+const as = require( 'wink-nlp/src/as.js' );
 const model = require('wink-eng-lite-model');
 const nlp = winkNLP(model);
 
-
-
 module.exports = (req, res) => {
   var doc = nlp.readDoc(req.query.sentence);
+
+  // Entities
+  var entities = doc.entities().out(its.detail);
+
+  // Counts
+  var sentences = doc.sentences().length();
+  var tokens = doc.tokens().length();
+  var words = doc.tokens().filter( (token) => {
+    return token.out(its.type) === 'word'
+  } ).length();
+
+  // Tagged text
+  var seenEntities = new Set();
+  doc.tokens().each( (token) => {
+    var entity = token.parentEntity();
+    if (entity === undefined) {
+      if (token.out(its.type) === 'word') {
+        token.markup('<span class=\"tag '+ token.out(its.pos) +'\">','</span>');
+      }
+    } else {
+      if (!seenEntities.has(entity.index())) {
+        entity.markup('<span class=\"tag '+ entity.out(its.type) +'\">', "</span>");
+      }
+      seenEntities.add(entity.index());
+    }
+  } )
+
+  // Word frequency
+  var wordFreq = doc.tokens().filter((token) => {
+    return token.out(its.type) === 'word' && !token.out(its.stopWordFlag);
+  }).out(its.normal, as.freqTable);
+  wordFreq = wordFreq.sort(function (a, b) {
+    return b[1] - a[1];
+  })
+  wordFreq = wordFreq.slice(0, 5)
+
+  // Sentiment
+  var sentiments = [];
+  doc.sentences().each((s) => {
+    sentiments.push({
+      sentence: s.out(),
+      sentiment: s.out(its.sentiment)
+    })
+  })
+
   res.json({
     sentence: req.query.sentence,
-    tags: doc.tokens().out(its.pos)
+    entities,
+    sentiments,
+    documentInfo: {
+      sentences,
+      tokens,
+      words
+    },
+    wordFreq,
+    taggedText: doc.out(its.markedUpText)
   })
 }
