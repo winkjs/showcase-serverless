96ed98ba3c99c676c96695b97187587d824469c6
Prateek Saxena <prtksxna@gmail.com>
2020-05-25 13:02:39 +0530

    fix(wp-timeline): use wink-nlp

--- wp-timeline.js
+++ wp-timeline.js
@@ -1,26 +1,25 @@
 const https = require('https');
 const fetch = require('node-fetch');
 
-const date = new RegExp(/[12][0-9]{3}/g);
+const winkNLP = require('wink-nlp');
+const its = require( 'wink-nlp/src/its.js' );
+const as = require( 'wink-nlp/src/as.js' );
+const model = require('wink-eng-lite-model');
+const nlp = winkNLP(model);
 
 module.exports = (req, res) => {
   var title = req.query.title;
-
   fetch('https://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles='+ title +'&explaintext=1&formatversion=2&format=json')
       .then(res => res.json())
       .then(json => json.query.pages[0].extract)
       .then(text => {
-        var sentences = text.replace( /\n/g, "." ).split('.');
-        sentences = sentences.map((s) => s.trim());
-        sentences = sentences.filter((s) => s.match(date) !== null );
-
+        var doc = nlp.readDoc(text);
         var timeline = {};
-        sentences.forEach((s) => {
-          var dates = s.match(date);
-          dates.forEach((d) => {
-            timeline[d] = s
-          });
-        });
+        doc.entities().each((e) => {
+          if (e.out(its.type) === 'DATE' ) {
+            timeline[e.out()] = e.parentSentence().out();
+          }
+        })
 
         res.json({
           title: req.query.title,
