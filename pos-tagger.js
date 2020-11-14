b71a304a9f23132f102f2979379ed39cfdb4b839
Prateek Saxena <prtksxna@gmail.com>
2020-11-14 12:09:50 +0530

    revert: last few commits around spaces

--- pos-tagger.js
+++ pos-tagger.js
@@ -23,13 +23,11 @@ module.exports = (req, res) => {
     var entity = token.parentEntity();
     if (entity === undefined) {
       if (token.out(its.type) === 'word') {
-        token.markup('<spanclass=\"tag '+ token.out(its.pos) +'\">','</span>');
-        // This will be replaced later with "span class" after spaces are replaced
+        token.markup('<span class=\"tag '+ token.out(its.pos) +'\">','</span>');
       }
     } else {
       if (!seenEntities.has(entity.index())) {
-        entity.markup('<spanclass=\"tag '+ entity.out(its.type) +'\">', "</span>");
-        // This will be replaced later with "span class" after spaces are replaced
+        entity.markup('<span class=\"tag '+ entity.out(its.type) +'\">', "</span>");
       }
       seenEntities.add(entity.index());
     }
@@ -60,9 +58,6 @@ module.exports = (req, res) => {
       words
     },
     wordFreq,
-    taggedText: doc.out(its.markedUpText)
-      .replace(/\n/g, '<br>')
-      .replace( / /g, '&nbsp;' )
-      .replace(/spanclass/g, 'span class')
+    taggedText: doc.out(its.markedUpText).replace(/\n/g, '<br>')
   })
 }
